2508276b191116d7e9e50a7376c362b59f8fd733
DBrush <coldnebraska@gmail.com>
2024-02-20 13:56:45 -0600

    added footer and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Header, Footer } from './components'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import { Outlet } from 'react-router-dom'
 import './App.css'
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
     return (
         <>
-            <ul>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
+            <ul className="footer-list">
+                <li><a href="https://github.com/coldnebraska">Github</a></li>
+                <li><a href="https://www.linkedin.com/in/brush-david/">LinkedIn</a></li>
+                <li><a href="https://twitter.com/DavidB11743">Twitter</a></li>
             </ul>
         </>
     )
