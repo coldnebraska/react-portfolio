6259b7e61c4944780acd4e1bc9ef51eef70bf2db
DBrush <coldnebraska@gmail.com>
2024-02-21 14:46:36 -0600

    added component for project loading

---
+++ src/components/Project.jsx
@@ -0,0 +1,75 @@
+export default function Project() {
+    const projects = [
+        {
+            sourceCode: 'https://github.com/coldnebraska/brushstrokes',
+            liveApp: 'https://brushconsult.com',
+            image: './src/assets/images/brushstrokes.png'
+        },
+        {
+            sourceCode: 'https://github.com/coldnebraska/gig-of-bands',
+            liveApp: 'https://gig-of-bandz-25bf9a236ebe.herokuapp.com',
+            image: './src/assets/images/gig-for-bands.png'
+        },
+        {
+            sourceCode: 'https://github.com/coldnebraska/travel-planner',
+            liveApp: 'https://coldnebraska.github.io/travel-planner/',
+            image: './src/assets/images/travel-planner.png'
+        },
+        {
+            sourceCode: 'https://github.com/coldnebraska/weather-dashboard',
+            liveApp: 'https://coldnebraska.github.io/weather-dashboard/',
+            image: './src/assets/images/weather.png'
+        },
+        {
+            sourceCode: 'https://github.com/coldnebraska/code-quiz',
+            liveApp: 'https://coldnebraska.github.io/code-quiz/',
+            image: './src/assets/images/code-quiz.png'
+        },
+        {
+            sourceCode: 'https://github.com/coldnebraska/css-test-page',
+            liveApp: 'https://coldnebraska.github.io/css-test-page/',
+            image: './src/assets/images/horiseon.png'
+        },
+    ]
+
+    return (
+        <ul>
+            <li>
+                <h1>BrushStrokes</h1>
+                <img src={projects[0].image}></img>
+                <a href={projects[0].liveApp}>Live Application</a>
+                <a href={projects[0].sourceCode}>Source Code</a>
+            </li>
+            <li>
+                <h1>Gigs for Bands</h1>
+                <img src={projects[1].image}></img>
+                <a href={projects[1].liveApp}>Live Application</a>
+                <a href={projects[1].sourceCode}>Source Code</a>
+            </li>
+            <li>
+                <h1>Travel Planner</h1>
+                <img src={projects[2].image}></img>
+                <a href={projects[2].liveApp}>Live Application</a>
+                <a href={projects[2].sourceCode}>Source Code</a>
+            </li>
+            <li>
+                <h1>Weather Dashboard</h1>
+                <img src={projects[3].image}></img>
+                <a href={projects[3].liveApp}>Live Application</a>
+                <a href={projects[3].sourceCode}>Source Code</a>
+            </li>
+            <li>
+                <h1>Code Quiz</h1>
+                <img src={projects[4].image}></img>
+                <a href={projects[4].liveApp}>Live Application</a>
+                <a href={projects[4].sourceCode}>Source Code</a>
+            </li>
+            <li>
+                <h1>Horiseon</h1>
+                <img src={projects[5].image}></img>
+                <a href={projects[5].liveApp}>Live Application</a>
+                <a href={projects[5].sourceCode}>Source Code</a>
+            </li>
+        </ul>
+    )
+}
